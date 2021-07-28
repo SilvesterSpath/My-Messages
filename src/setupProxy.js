ec7a615fd79f6f5fff2110a90704e315ebe7a907
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-07-28 07:02:01 +0200

    ./*

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,5 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
-  app.use(
-    createProxyMiddleware('/api/*', { target: 'http://localhost:5000/' })
-  );
+  app.use(createProxyMiddleware('/*', { target: 'http://localhost:5000/' }));
 };
